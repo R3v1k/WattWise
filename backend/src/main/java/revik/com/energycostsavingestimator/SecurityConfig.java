71f3a569da77335814538070ca374ae8937a436b
Brevolg <102014222+Brevolg@users.noreply.github.com>
2025-07-06 23:38:08 +0300

    Update SecurityConfig.java

--- backend/src/main/java/revik/com/energycostsavingestimator/SecurityConfig.java
+++ backend/src/main/java/revik/com/energycostsavingestimator/SecurityConfig.java
@@ -2,42 +2,25 @@ package revik.com.energycostsavingestimator;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
-import revik.com.energycostsavingestimator.config.JwtFilter;
 
 @Configuration
+@Profile("!test") 
 public class SecurityConfig {
-
-    private final JwtFilter jwtFilter;
-
-    public SecurityConfig(JwtFilter jwtFilter) {
-        this.jwtFilter = jwtFilter;
-    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
-                .csrf(csrf -> csrf.disable())
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(
-                                "/auth/**",
-                                "/swagger-ui.html",
-                                "/swagger-ui/**",
-                                "/v3/api-docs/**",
-                                "/swagger-resources/**",
-                                "/webjars/**",
-                                "/favicon.ico",
-                                "/error")
-                        .permitAll()
-                        .anyRequest().authenticated()
-                )
-                .addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class)
-                .formLogin(AbstractHttpConfigurer::disable)
-                .httpBasic(AbstractHttpConfigurer::disable);
-
+            .csrf(csrf -> csrf.disable())
+            .authorizeHttpRequests(auth -> auth
+                    .requestMatchers("/api/public/**").permitAll()
+                    .anyRequest().authenticated()
+            )
+            .formLogin()
+            .and()
+            .httpBasic();
         return http.build();
     }
 }
