75f3c2afb5df3e3da5fe1559ea2ccb56c5dcc13b
Brevolg <102014222+Brevolg@users.noreply.github.com>
2025-07-06 23:22:08 +0300

    Update EnergyCostSavingEstimatorApplicationTests.java

--- backend/src/test/java/revik/com/energycostsavingestimator/EnergyCostSavingEstimatorApplicationTests.java
+++ backend/src/test/java/revik/com/energycostsavingestimator/EnergyCostSavingEstimatorApplicationTests.java
@@ -3,11 +3,15 @@ package revik.com.energycostsavingestimator;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.context.annotation.Import;
 
 @SpringBootTest
 @ActiveProfiles("test")
+@Import(TestSecurityConfig.class)
 class EnergyCostSavingEstimatorApplicationTests {
+
     @Test
     void contextLoads() {
     }
+
 }
