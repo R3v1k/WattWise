374a4f3756e88c762348c917ec5bcbbb6d860609
etern1ty22 <m.akhmarov@innopolis.university>
2025-07-10 15:14:00 +0300

    files moved

--- backend/src/test/java/TestSecurityConfig.java
+++ backend/src/test/java/TestSecurityConfig.java
@@ -1,5 +1,3 @@
-package revik.com.energycostsavingestimator;
-
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Primary;
